b90c411dd23f326df2fb9840d5bda82cfd612695
ashton mupereki <mup23001@byui.edu>
2023-06-20 14:36:01 +0200

    added

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,21 +2,12 @@ using System;
 
 namespace MyNamespace
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the goal tracker program!");
-
-            Menu menu = new Menu();
-            menu.DisplayChoices();
-            menu.GetChoices();
-
-            
             Console.WriteLine("Thank you for using the goal tracker program!");
-                        
         }
     }
 }
-    
-
